6e61e91def2451d72da617aa9febafff129b7bc9
princeantwi2k <antwi2k@gmail.com>
2021-10-09 11:25:20 +0800

    added App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter,Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Register from './Pages/Register';
-import PriveRoute from './Component/PriveRoute';
 import "./App.css";
 import Login from './Pages/Login';
+import PrivateRoute from './Component/PrivateRoute';
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <PriveRoute exact path="/" component={Home} />
+      <PrivateRoute exact path="/" component={Home} />
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
       </BrowserRouter>
